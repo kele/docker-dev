fe3127109afe773792aa9d6bde5d7533ef97d9da
kele <damian.bogel@gmail.com>
2018-04-08 17:31:04 +0200

    Added a message about needing to use sudo to run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,8 @@
+echo "========================================================================================="
+echo "You most likely need to run this script with sudo, unless you belong to the docker group."
+echo "See: https://docs.docker.com/engine/security/security/#docker-daemon-attack-surface"
+echo "========================================================================================="
+echo
+
 docker build -t dev .
 docker run -it -e "TERM=xterm-256color" dev /bin/zsh
